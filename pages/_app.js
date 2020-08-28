5d3b0519219a02980255da6ee9e1c773be084c89
handrei <h.andrei92@gmail.com>
2020-08-28 13:56:18 +0300

    first-commit

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import '../styles/globals.css';
+import App from 'next/app';
+import { appWithTranslation } from '../i18n';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+MyApp.getInitialProps = async (appContext) => ({
+  ...(await App.getInitialProps(appContext)),
+});
+
+export default appWithTranslation(MyApp);
